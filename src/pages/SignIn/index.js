3ae3d19736bc844a55bcc2d463ddb9bc0969e93e
Dheimison Siqueira <dheimisonlo@gmail.com>
2020-05-01 15:47:54 -0300

    feat: adds folder of auth and default layouts and their settings

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -4,7 +4,7 @@ import * as Yup from 'yup';
 
 import { signInRequest } from '../../store/modules/auth/actions';
 
-import { Container, Label } from './styles';
+import { Label } from './styles';
 
 import logo from '../../assets/logo.png';
 
@@ -62,38 +62,36 @@ export default function SignIn() {
   }
 
   return (
-    <Container>
-      <div>
-        <img src={logo} alt="Logo fastfeet" />
+    <div>
+      <img src={logo} alt="Logo fastfeet" />
 
-        <form onSubmit={handleSubmit}>
-          <Label htmlFor="email" error={emailError}>
-            Seu E-mail
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="exemplo@email.com"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Label>
+      <form onSubmit={handleSubmit}>
+        <Label htmlFor="email" error={emailError}>
+          Seu E-mail
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="exemplo@email.com"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Label>
 
-          <Label htmlFor="password" error={passwordError}>
-            Sua Senha
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="******"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Label>
+        <Label htmlFor="password" error={passwordError}>
+          Sua Senha
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="******"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Label>
 
-          <button type="submit">
-            {loading ? 'Carregando...' : 'Entrar no sistema'}
-          </button>
-        </form>
-      </div>
-    </Container>
+        <button type="submit">
+          {loading ? 'Carregando...' : 'Entrar no sistema'}
+        </button>
+      </form>
+    </div>
   );
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,46 +1,5 @@
 import styled, { keyframes } from 'styled-components';
 
-export const Container = styled.div`
-  height: 100%;
-  background: #7d40e7;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  div {
-    height: 425px;
-    width: 360px;
-    background: #fff;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-
-    img {
-      width: 260px;
-      margin-bottom: 45px;
-    }
-
-    form {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-
-      button {
-        width: 300px;
-        height: 45px;
-        background: #7d40e7;
-        border-radius: 4px;
-        border: none;
-        color: #fff;
-        font-size: 16px;
-        font-weight: bold;
-        margin-top: 15px;
-      }
-    }
-  }
-`;
-
 const fadeIn = keyframes`
  from {
    opacity: 0;
