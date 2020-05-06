a575999a1db3249b379b8559200a6815950efbe8
Dheimison Siqueira <dheimisonlo@gmail.com>
2020-05-06 18:45:45 -0300

    chore: creates a file with standard colors

---
+++ src/styles/colors.js
@@ -0,0 +1,28 @@
+const colors = [
+  {
+    primary: '#a28fd0',
+    secondary: '#f4effc',
+  },
+  {
+    primary: '#cb946c',
+    secondary: '#fcf4ee',
+  },
+  {
+    primary: '#83cec9',
+    secondary: '#ebfbfa',
+  },
+  {
+    primary: '#cc7584',
+    secondary: '#ffeef1',
+  },
+  {
+    primary: '#a8d080',
+    secondary: '#f4f9ef',
+  },
+  {
+    primary: '#cccc8b',
+    secondary: '#fcfcef',
+  },
+];
+
+export default colors;
